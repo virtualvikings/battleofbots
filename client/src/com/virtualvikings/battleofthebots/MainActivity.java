953ab3d6b7009beb04a2760a7e6b92b12af4491c
githubsven <s.luehof@gmail.com>
2014-11-24 12:18:05 +0100

    Client

--- client/src/com/virtualvikings/battleofthebots/MainActivity.java
+++ client/src/com/virtualvikings/battleofthebots/MainActivity.java
@@ -4,14 +4,19 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	Button matchBtn, editBtn;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        matchBtn = (Button) findViewById(R.id.matchBtn);
+        editBtn = (Button) findViewById(R.id.editBtn);
     }
 
 
