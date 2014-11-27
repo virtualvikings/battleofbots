b1fcc8d242e0b058627c00fff491c01c75fa5b8e
Roald <r.melssen@students.uu.nl>
2014-11-27 16:43:24 +0100

    Testveld toegevoegd

--- client/src/com/virtualvikings/battleofthebots/GameView.java
+++ client/src/com/virtualvikings/battleofthebots/GameView.java
@@ -1,5 +1,7 @@
 package com.virtualvikings.battleofthebots;
 
+import java.util.Random;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -10,36 +12,73 @@ import android.widget.LinearLayout;
 
 public class GameView extends View {
 
+	Paint brush = new Paint();
+	int cellCount = 10;
+	int[][] cells;
+	
 	public GameView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
+		
+		cells = new int[cellCount][cellCount];
 		invalidate();
+		
+		//Maak willekeurig test level
+		Random r = new Random();
+		for (int i = 0; i < cellCount; i++)
+		{
+			for (int j = 0; j < cellCount; j++)
+			{
+				cells[i][j] = r.nextInt(7);
+			}
+		}
+		
 		
 	}
 	
-	Paint brush = new Paint();
-	int cells = 10;
+
 	
 	@Override
 	public void onDraw (Canvas canvas)
 	{
 		brush.setStrokeWidth(2);
-		brush.setColor(Color.WHITE);
+		brush.setAntiAlias(true);
 		
+		//Bepaal of breedte of hoogte kleiner is
 		int minWH = Math.min(canvas.getWidth(), canvas.getHeight());
-		float cellS = minWH / (float)cells;
+		float cellS = minWH / (float)cellCount;
 		
+		//Centreer
 		if (minWH == canvas.getWidth())
 			canvas.translate(0, canvas.getHeight() / 2f - minWH / 2f);
 		else
 			canvas.translate(canvas.getWidth() / 2f - minWH / 2f, 0);
-
-		for (int i = 0; i <= cells; i++)
+		
+		//Teken lijnen
+		brush.setColor(Color.GREEN);
+		for (int i = 0; i <= cellCount; i++)
 		{
-			for (int j = 0; j <= cells; j++)
+			for (int j = 0; j <= cellCount; j++)
 			{
-				canvas.drawLine(i * cellS, 0, i * cellS, minWH, brush);
-				canvas.drawLine(0, j * cellS, minWH, j * cellS, brush);
+				float x = i * cellS;
+				float y = j * cellS;
+				
+				canvas.drawLine(x, 0, x, minWH, brush);
+				canvas.drawLine(0, y, minWH, y, brush);
+			}
+		}
+
+		//Teken obstakels enzo
+		brush.setColor(Color.WHITE);
+		for (int i = 0; i < cellCount; i++)
+		{
+			for (int j = 0; j < cellCount; j++)
+			{
+				float x = i * cellS;
+				float y = j * cellS;
+					
+				if (cells[i][j] == 0)
+					canvas.drawCircle(x + cellS / 2f, y + cellS / 2f, cellS / 2f, brush);
 			}
 		}
 	}
