78e2723bcfc2d3183a14bef695c3745245a51030
Roald <r.melssen@students.uu.nl>
2014-12-02 12:48:10 +0100

    GameView kan nu de plek van de speler/vijand laten zien door de tijd heen

--- client/src/com/virtualvikings/battleofthebots/GameActivity.java
+++ client/src/com/virtualvikings/battleofthebots/GameActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toolbar.LayoutParams;
 
 public class GameActivity extends Activity {
 
@@ -12,12 +16,35 @@ public class GameActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
 		
-		RelativeLayout layout = (RelativeLayout) findViewById(R.id.test);
+		LinearLayout layout = (LinearLayout) findViewById(R.id.test);
 
-		GameView game = new GameView(getApplicationContext());
-		game.setLayoutParams(new LinearLayout.LayoutParams(
-		                                     LinearLayout.LayoutParams.MATCH_PARENT,
-		                                     LinearLayout.LayoutParams.MATCH_PARENT));
+		final GameView game = new GameView(getApplicationContext());
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+		params.gravity = 1;
+		game.setLayoutParams(params);
+		
 		layout.addView(game);
+		
+		final TextView text = (TextView) findViewById(R.id.textTime);
+		SeekBar bar = (SeekBar) findViewById(R.id.seekBar);
+		bar.setMax(game.getTimeSegments() - 1); 
+		
+		bar.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				game.setProgress(progress);
+				text.setText(String.format("Time: %d/%d", progress + 1, game.getTimeSegments()));
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}});
 	}
 }
--- client/src/com/virtualvikings/battleofthebots/GameView.java
+++ client/src/com/virtualvikings/battleofthebots/GameView.java
@@ -1,6 +1,8 @@
 package com.virtualvikings.battleofthebots;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -8,28 +10,100 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
+class Vector2<E>
+{
+	public E x;
+	public E y;
+	public Vector2(E x, E y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public boolean equals(Vector2<E> other) {
+		return other.x.equals(x) && other.y.equals(y);
+	}
+}
+
 public class GameView extends View {
+	
+	class Bot
+	{
+		Vector2<Integer>[] positions;
+		byte[] directions; //0-3
+		
+		public Bot(Vector2<Integer>[] positions) {
+			this.positions = positions;
+		}
+	}
 
 	Paint brush;
 	int cellCount = 10;
-	int[][] cells;
+	int timeSegments;
+	int currentTime;
+	byte[][][] cells;
+	
+	Bot player;
+	Bot enemy;
+	
+	public int getTimeSegments() {
+		return timeSegments;
+	}
+	
+
+	public void setProgress(int progress) {
+		currentTime = progress;
+		invalidate();
+	}
 	
 	public GameView(Context context) {
 		super(context);
 		
-		cells = new int[cellCount][cellCount];
+		timeSegments = 10;
+		cells = new byte[cellCount][cellCount][timeSegments];
+		
+		//Plaats bots op willekeurige plekken
+		Random r = new Random();
+		Vector2<?>[] positionsPlayer = new Vector2<?>[timeSegments];
+		Vector2<?>[] positionsEnemy = new Vector2<?>[timeSegments];
+		for (int i = 0; i < timeSegments; i++) {
+			positionsPlayer[i] = new Vector2<Integer>(r.nextInt(cellCount), r.nextInt(cellCount));
+			positionsEnemy[i] = new Vector2<Integer>(r.nextInt(cellCount), r.nextInt(cellCount));
+			System.out.println("player[" + i + "]: " + positionsPlayer[i].x + ", " + positionsPlayer[i].y);
+			System.out.println("enemy[" + i + "]: " + positionsEnemy[i].x + ", " + positionsEnemy[i].y);
+		}
+		
+		player = new Bot((Vector2<Integer>[]) positionsPlayer);
+		enemy = new Bot((Vector2<Integer>[]) positionsEnemy);
 		
 		//Maak willekeurig test level
-		Random r = new Random();
 		for (int i = 0; i < cellCount; i++)
 			for (int j = 0; j < cellCount; j++)
-				cells[i][j] = r.nextInt(7);
+				for (int k = 0; k < timeSegments; k++)
+				{
+					byte value = (byte) r.nextInt(7);
+					if (k > 0) //Alle andere arrays kopieren de eerste
+						value = cells[i][j][0];
+					cells[i][j][k] = value;
+				}
 		
 		brush = new Paint();
 		brush.setStrokeWidth(2);
 		brush.setAntiAlias(true);
 		
 		invalidate();
+		
+		/*Timer t = new Timer();
+		TimerTask task = new TimerTask(){
+			@Override
+			public void run() {
+				GameView.this.post(new Runnable(){
+					@Override
+					public void run() {
+						GameView.this.invalidate();
+					}});
+			}};
+		t.schedule(task , 0, 1000); //Doe dit elke seconde*/
 	}
 	
 	@Override
@@ -46,7 +120,7 @@ public class GameView extends View {
 			canvas.translate(canvas.getWidth() / 2f - minWH / 2f, 0);
 		
 		//Teken lijnen
-		brush.setColor(Color.GREEN);
+		brush.setColor(Color.rgb(100, 100, 100));
 		for (int i = 0; i <= cellCount; i++)
 		{
 			for (int j = 0; j <= cellCount; j++)
@@ -59,16 +133,24 @@ public class GameView extends View {
 			}
 		}
 
-		//Teken obstakels enzo
-		brush.setColor(Color.WHITE);
+		//Teken obstakels en bots
 		for (int i = 0; i < cellCount; i++)
 		{
 			for (int j = 0; j < cellCount; j++)
 			{
 				float x = i * cellS;
 				float y = j * cellS;
-					
-				if (cells[i][j] == 0)
+				Vector2<Integer> pos = new Vector2<Integer>(i, j);
+				
+				brush.setColor(Color.WHITE);
+				if (cells[i][j][currentTime] == 0)
+					canvas.drawCircle(x + cellS / 2f, y + cellS / 2f, cellS / 2f, brush);
+				
+				brush.setColor(Color.GREEN);
+				if (player.positions[currentTime].equals(pos))
+					canvas.drawCircle(x + cellS / 2f, y + cellS / 2f, cellS / 2f, brush);
+				brush.setColor(Color.RED);
+				if (enemy.positions[currentTime].equals(pos))
 					canvas.drawCircle(x + cellS / 2f, y + cellS / 2f, cellS / 2f, brush);
 			}
 		}
