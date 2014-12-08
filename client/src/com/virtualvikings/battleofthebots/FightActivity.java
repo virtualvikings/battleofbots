00768faa3a8cac438f4036e39bcc72cf79cbc45d
githubsven <s.luehof@gmail.com>
2014-12-08 13:18:21 +0100

    Nieuwe client
    YAAAY

---
+++ client/src/com/virtualvikings/battleofthebots/FightActivity.java
@@ -0,0 +1,16 @@
+package com.virtualvikings.battleofthebots;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FightActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fight);
+	}
+
+	
+}
--- client/src/com/virtualvikings/battleofthebots/MatchMaking.java
+++ client/src/com/virtualvikings/battleofthebots/MatchMaking.java
@@ -1,72 +1,58 @@
 package com.virtualvikings.battleofthebots;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MatchMaking extends Activity {
+public class MatchMaking extends ActionBarActivity {
+	
+	TextView matchTxt;
+	Boolean matchFound = false;
+	
+	final String FileName = "Strategy";
+	String code;
+	
+	final int port = 4444;
+	final String IP = "145.107.119.237";
+	Boolean connected = false;
+	Socket socket;
+	PrintWriter out;
+	BufferedReader in;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_match);
+		matchTxt = (TextView) findViewById(R.id.matchTxt);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		
-		final ProgressDialog ringProgressDialog = ProgressDialog.show(this, "Please wait",	"Searching for match...", true);
-		ringProgressDialog.setCancelable(true);
-
-		//Tijdelijk: ga naar spel activity (zelfs als verbinding niet gelukt is)
-		Intent goToNextActivity = new Intent(getApplicationContext(), GameActivity.class);
-		startActivity(goToNextActivity);
-		
-		new Thread(new Runnable(){
-			
-			private void makeToast(final String s)
-			{
-				MatchMaking.this.runOnUiThread(new Runnable() {
-				    public void run() {
-				        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
-				    }
-				});
-			}
-
-			@Override
-			public void run() {
-				
-				makeToast("Starten...");
-				
-				try {
-					
-					//"172.20.10.2"
-					InetAddress serverAddr = InetAddress.getByName("10.0.2.2");
-					//Dat werkt!
-
-					Socket socket = new Socket(serverAddr, 4444);
-					
-					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-					out.println("Correct_Key");
-					socket.close();
-					
-					makeToast("Voltooid.");
-
-				} catch (Exception e1) {
-					e1.printStackTrace();
-					makeToast("Fout opgetreden.");
-				}
-				
-			}}).start();
+		SharedPreferences readCode = getSharedPreferences(FileName, MODE_PRIVATE);
+		code = readCode.getString("code", "");
 	}
 	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		TalkToServer.start();
+	}
+
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
+			closeConnection();
 			finish();
 			return true;
 
@@ -74,5 +60,56 @@ public class MatchMaking extends Activity {
 			return false;
 		}
 	}
+	
+	Thread TalkToServer = new Thread(){
+		
+		public void run(){
+			try {
+				InetAddress serverAddr = InetAddress.getByName(IP);
+				socket = new Socket(serverAddr, port);
+				out = new PrintWriter(socket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				connected = true;
+				//matchTxt.setText("Searching opponent");
+			} catch (IOException e) {
+				Toast.makeText(getApplicationContext(), "Could not connect to server, please try again later", Toast.LENGTH_LONG).show();
+				finish();
+			}
+			
+			try {
+				String fromServer;
+				while ((fromServer = in.readLine()) != null) {
+						if(fromServer.equals("requestKey"))
+							out.println("Correct_Key");
+						else if(fromServer.equals("requestData"))
+							out.println("poep, " + code);
+						else if(fromServer.equals("matchFound")){
+							runOnUiThread(new Runnable() {
 
+		                        @Override
+		                        public void run() {
+		                            Intent i = new Intent("android.intent.action.FIGHTACTIVITY");
+		                            startActivity(i);
+		                            finish();
+		                        }
+		                    });
+						}
+				}
+			} catch (IOException e) {
+				Toast.makeText(getApplicationContext(), "Could not transmit data to server, please try again later", Toast.LENGTH_LONG).show();
+				finish();
+			}
+		}
+	};
+	
+	public void closeConnection() {
+		if (connected) {
+			try {
+				out.println("exit");
+				socket.close();
+			} catch (IOException e) {
+				Toast.makeText(getApplicationContext(), "Could not close the connection", Toast.LENGTH_SHORT).show();
+			}
+		} 
+	}
 }
