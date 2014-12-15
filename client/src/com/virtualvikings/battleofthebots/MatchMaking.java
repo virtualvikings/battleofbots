26ffcdfdb14203309e1227b5942a332e59cef159
githubsven <s.luehof@gmail.com>
2014-12-15 11:54:54 +0100

    Nieuwe MatchMaking

--- client/src/com/virtualvikings/battleofthebots/MatchMaking.java
+++ client/src/com/virtualvikings/battleofthebots/MatchMaking.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 
 import android.content.Intent;
@@ -25,7 +24,7 @@ public class MatchMaking extends ActionBarActivity {
 	String code;
 	
 	final int port = 4444;
-	final String IP = "10.0.2.2";//"145.107.119.237";
+	final String IP = "145.107.118.39";
 	Boolean connected = false;
 	Socket socket;
 	PrintWriter out;
@@ -62,67 +61,46 @@ public class MatchMaking extends ActionBarActivity {
 		}
 	}
 	
-	
-	private void makeToast(final String s)
-	{
-		runOnUiThread(new Runnable() {
-		    public void run() {
-		        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
-		    }
-		});
-	}
-	
 	Thread TalkToServer = new Thread(){
-
+		
 		public void run(){
 			try {
 				InetAddress serverAddr = InetAddress.getByName(IP);
-				makeToast("Connecting...");
-				
-				int timeout = 10 * 1000; //Wacht 10 sec
-				socket = new Socket();
-				socket.connect(new InetSocketAddress(serverAddr, port), timeout);
-				
+				socket = new Socket(serverAddr, port);
 				out = new PrintWriter(socket.getOutputStream(), true);
 				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				connected = true;
-				//matchTxt.setText("Searching opponent");
-				
-				try {
-					makeToast("Searching opponent...");
-					String fromServer;
-					while ((fromServer = in.readLine()) != null) {
-							if(fromServer.equals("requestKey"))
-								out.println("Correct_Key");
-							else if(fromServer.equals("requestData"))
-								out.println("poep, " + code);
-							else if(fromServer.equals("matchFound") || true){
-								runOnUiThread(new Runnable() {
-
-			                        @Override
-			                        public void run() {
-			                            Intent i = new Intent("android.intent.action.FIGHTACTIVITY");
-			                            i.putExtra("mapData", "TODO put map data here");
-			                            startActivity(i);
-			                            finish();
-			                        }
-			                    });
-							}
-							else if (fromServer.equals("Server too busy, try again later."))
-								throw new IOException(fromServer);
-							else
-								System.out.println("Received misc data: " + fromServer);
-					}
-				} catch (IOException e) {
-					makeToast("Could not transmit data to server: " + e.getMessage());
-					finish();
-				}
-				
 			} catch (IOException e) {
-				makeToast("Could not connect to server: " + e.getMessage());
+				Toast.makeText(getApplicationContext(), "Could not connect to server, please try again later", Toast.LENGTH_LONG).show();
 				finish();
 			}
-		
+			
+			try {
+				String fromServer;
+				while ((fromServer = in.readLine()) != null) {
+						if(fromServer.equals("requestKey"))
+							out.println("Correct_Key");
+						else if(fromServer.equals("requestData"))
+							out.println("poep, " + code);
+						else if(fromServer.startsWith("matchFound")){
+							final String mapData = fromServer.substring(10, fromServer.length());
+							closeConnection();
+							runOnUiThread(new Runnable() {
+
+		                        @Override
+		                        public void run() {
+		                            Intent i = new Intent("android.intent.action.GAMEACTIVITY");
+		                            i.putExtra("mapData", mapData);
+		                            startActivity(i);
+		                            finish();
+		                        }
+		                    });
+						}
+				}
+			} catch (IOException e) {
+				Toast.makeText(getApplicationContext(), "Could not transmit data to server, please try again later", Toast.LENGTH_LONG).show();
+				finish();
+			}
 		}
 	};
 	
@@ -132,7 +110,7 @@ public class MatchMaking extends ActionBarActivity {
 				out.println("exit");
 				socket.close();
 			} catch (IOException e) {
-				makeToast("Could not close the connection");
+				Toast.makeText(getApplicationContext(), "Could not close the connection", Toast.LENGTH_SHORT).show();
 			}
 		} 
 	}
