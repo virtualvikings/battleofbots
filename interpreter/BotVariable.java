66c22f2744b893ac1c82c2b10e39a2dc7ab8966e
Squarion <github@davidlieffijn.nl>
2015-01-22 13:40:24 +0100

    Added BotVariable

---
+++ interpreter/BotVariable.java
@@ -0,0 +1,22 @@
+package nl.davidlieffijn.battleofbots.interpreter;
+
+import java.util.Random;
+
+public class BotVariable extends Variable {
+	public BotVariable(String name) {
+		super(name, new Number(0));
+	}
+	
+		
+	public int result(byte[] stats) {
+		switch (name) {
+		case "DIRECTION": return stats[0];
+		case "HP": return stats[1];
+		case "POS-X": return stats[2];
+		case "POS-Y": return stats[3];
+		case "RANDOM": return new Random().nextInt(100);
+		case "VIEW": return stats[5];
+		}
+		return -1;
+	}
+}
