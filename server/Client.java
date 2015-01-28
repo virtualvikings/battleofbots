1da7c8a6245d86f32c59d79b44f4580c407cfd2e
Squarion <github@davidlieffijn.nl>
2015-01-28 18:01:50 +0100

    More cleaning up

--- server/Client.java
+++
@@ -1,8 +0,0 @@
-package nl.davidlieffijn.battleofbots;
-
-public class Client {
-	public static void main(String[] args) {
-		new ClientTest("localhost", 4444);
-	}
-	
-}
--- server/Server.java
+++
@@ -1,9 +0,0 @@
-package nl.davidlieffijn.battleofbots;
-
-import java.io.IOException;
-
-public class Server {
-	public static void main(String[] args) throws IOException {
-		MultiServerTest mst = new MultiServerTest(4444);
-	}
-}
